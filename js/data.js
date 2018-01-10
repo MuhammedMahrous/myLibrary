af3787af67856cddfe47c44d477dbd24b25dbabf
Ahmed Nagib <a.nagib@centione.com>
2018-01-10 17:43:04 +0200

    edit function getShlefs

--- js/data.js
+++ js/data.js
@@ -29,11 +29,10 @@ function findByBookTitle(element) {
 function findByID(element) {
     return element.id == noteID;
 }
-
 /* 
-    Utility function used to validate username 
-    return true if validated and false if not
-*/
+ Utility function used to validate username 
+ return true if validated and false if not
+ */
 function validateUsername(userName = getCurrentUser()) {
     var validated;
 
@@ -73,9 +72,9 @@ function getCurrentUser() {
 
 
 /* 
-    Sets the current username to logged in user
-    returns true if successfully set and false if not.
-*/
+ Sets the current username to logged in user
+ returns true if successfully set and false if not.
+ */
 
 function setCurrentUser(userName) {
     // Validation to prevent setting user to any random user
@@ -90,7 +89,7 @@ function setCurrentUser(userName) {
 /* NOTES DATA */
 
 /*
-    returns from localStorage an array the notes of a user 
+ returns from localStorage an array the notes of a user 
  */
 function getNotes(userName = getCurrentUser()) {
     var userNotes;
@@ -130,9 +129,9 @@ function getNotesByBookTitle(bookTitle, userName = getCurrentUser()) {
 }
 
 /* 
-    sets a note into account of a specific user
-    if note exists it will be overwritten
-*/
+ sets a note into account of a specific user
+ if note exists it will be overwritten
+ */
 function setNote(note, userName = getCurrentUser()) {
     var userNotes;
 
@@ -187,8 +186,8 @@ function setNote(note, userName = getCurrentUser()) {
 }
 
 /* 
-    Remove a note by note id and username
-*/
+ Remove a note by note id and username
+ */
 function deleteNoteById(noteid, userName = getCurrentUser()) {
     var userNotes;
 
@@ -202,8 +201,8 @@ function deleteNoteById(noteid, userName = getCurrentUser()) {
     // if note already exists then replace delete it from array
     if (index != -1) {
 
-        userNotes.splice(index,1);
-    }    
+        userNotes.splice(index, 1);
+    }
 
     // put notes back into storage
 
@@ -243,7 +242,7 @@ function createShlef(username, title) {
 
     // create localStorage if it does not exist
     if (!localStorage.getItem('shelfs')) {
-        localStorage.setItem('shelfs', JSON.stringify([{ "username": username, "userShelfs": [] }]));
+        localStorage.setItem('shelfs', JSON.stringify([{"username": username, "userShelfs": []}]));
     }
 
     var allShelfs = JSON.parse(localStorage.getItem("shelfs"));
@@ -252,7 +251,7 @@ function createShlef(username, title) {
 
     // create data for user for the first 
     if (userShelfIndex == -1) {
-        allShelfs.push({ "username": username, "userShelfs": [] })
+        allShelfs.push({"username": username, "userShelfs": []})
         localStorage.setItem('shelfs', JSON.stringify(allShelfs));
         userShelfIndex = allShelfs.findIndex(shelf => shelf.username == username);
     }
@@ -278,107 +277,18 @@ function createShlef(username, title) {
 
 function getShlefs(username) {
     var allShelfs = JSON.parse(localStorage.getItem("shelfs"));
-    if (allShelfs.length > 0) {
-        var userShelfs = allShelfs.filter(shelf => shelf.username == username);
-        if (userShelfs.length > 0)
-            return userShelfs[0].userShelfs
-        else
-            return false;
-    } else
-        return false;
-}
-
-function getShlef(username, title) {
-    var allShelfs = JSON.parse(localStorage.getItem("shelfs"));
-    if (allShelfs.length > 0) {
-        var userShelfIndex = allShelfs.findIndex(shelf => shelf.username == username);
-        if (userShelfIndex != -1) {
-            var shelf = allShelfs[userShelfIndex].userShelfs.filter(shelf => shelf.title == title);
-            return shelf[0].books;
+    if (allShelfs != null) {
+        if (allShelfs.length > 0) {
+            var userShelfs = allShelfs.filter(shelf => shelf.username == username);
+            if (userShelfs.length > 0)
+                return userShelfs[0].userShelfs
+            else
+                return false;
         } else
             return false;
-    } else
-        return false;
-}
-
-function setBook(username, shelfTitle, book) {
-    var allShelfs = JSON.parse(localStorage.getItem("shelfs"));
-    if (allShelfs.length > 0) {
-        var userShelfIndex = allShelfs.findIndex(shelf => shelf.username == username);
-        if (userShelfIndex != -1) {
-            allShelfs[userShelfIndex].userShelfs.filter(shelf => shelf.title == shelfTitle)[0].books.push(book);
-            localStorage.setItem('shelfs', JSON.stringify(allShelfs));
-            return true;
-        } else
-            return false;
-    } else
-        return false;
-}
-
-function deleteBook(username, shelfTitle, BookPath) {
-    var allShelfs = JSON.parse(localStorage.getItem("shelfs"));
-    if (allShelfs.length > 0) {
-        var userShelfIndex = allShelfs.findIndex(shelf => shelf.username == username);
-        if (userShelfIndex != -1) {
-            var books = allShelfs[userShelfIndex].userShelfs.filter(shelf => shelf.title == shelfTitle)[0].books;
-            var index = allShelfs[userShelfIndex].userShelfs.filter(shelf => shelf.title == shelfTitle)[0].books.findIndex(book => book.filePath == BookPath);
-            books.splice(index, 1);
-            return true;
-        } else
-            return false;
-    } else
-        return false;
-}
-
-
-function createShlef(username, title) {
-
-
-    // create localStorage if it does not exist
-    if (!localStorage.getItem('shelfs')) {
-        localStorage.setItem('shelfs', JSON.stringify([{ "username": username, "userShelfs": [] }]));
-    }
-
-    var allShelfs = JSON.parse(localStorage.getItem("shelfs"));
-    var userShelfIndex = allShelfs.findIndex(shelf => shelf.username == username);
-
-
-    // create data for user for the first 
-    if (userShelfIndex == -1) {
-        allShelfs.push({ "username": username, "userShelfs": [] })
-        localStorage.setItem('shelfs', JSON.stringify(allShelfs));
-        userShelfIndex = allShelfs.findIndex(shelf => shelf.username == username);
-    }
-
-    // check if shelf already exists
-    if (allShelfs[userShelfIndex].userShelfs.filter(shelf => shelf.title == title).length > 0) {
-        return false;
     } else {
-        newShlef = {
-            "title": title,
-            "books": []
-        };
-        allShelfs[userShelfIndex].userShelfs.push(newShlef);
-
-        //update all shelfs data 
-        localStorage.setItem('shelfs', JSON.stringify(allShelfs));
-
-        return true;
-
-    }
-
-}
-
-function getShlefs(username) {
-    var allShelfs = JSON.parse(localStorage.getItem("shelfs"));
-    if (allShelfs.length > 0) {
-        var userShelfs = allShelfs.filter(shelf => shelf.username == username);
-        if (userShelfs.length > 0)
-            return userShelfs[0].userShelfs
-        else
-            return false;
-    } else
         return false;
+    }
 }
 
 function getShlef(username, title) {
@@ -395,7 +305,9 @@ function getShlef(username, title) {
 }
 
 function setBook(username, shelfTitle, book) {
+    
     var allShelfs = JSON.parse(localStorage.getItem("shelfs"));
+
     if (allShelfs.length > 0) {
         var userShelfIndex = allShelfs.findIndex(shelf => shelf.username == username);
         if (userShelfIndex != -1) {
